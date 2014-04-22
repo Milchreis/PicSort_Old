844e8dcb4fbf87a39de37a0119dc0d00a0210df2
Nick <nick-m@gmx.de>
2014-04-22 19:15:26 +0200

    Change: automatic language detection and changing by os defaults.

--- src/de/nartis/lang/Lang.java
+++ src/de/nartis/lang/Lang.java
@@ -2,11 +2,46 @@ package de.nartis.lang;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Lang {
 	
 	private static Properties props;
+	private static Map<String, String> map;
+	
+	
+	public static void addLanguage( String locale, String name) {
+		
+		if(map == null) 
+			map = new HashMap<String, String>();
+		
+		map.put( locale, name );
+	}
+	
+	
+	public static void setLanguageByOs() {
+
+		if( map == null )
+			throw new IllegalStateException( "Please add possible languages before using: Lang.addLanguage(\"english\")" );
+		
+		String locale = System.getProperty("user.language");
+		
+		String file = map.get( locale );
+			
+		try {
+
+			if( file != null )
+				Lang.setLanguage( file );
+
+		} catch( Exception e ) {
+			e.printStackTrace();
+		}
+	}
 	
 	public static void setLanguage( String language ) throws FileNotFoundException, IOException {
 		props = new Properties();
@@ -30,6 +65,29 @@ public class Lang {
 			return props.get( key ).toString();
 		} catch( Exception e ) {
 			return key;
+		}
+	}
+	
+	public static void openChooser( JFrame window, String[] languages ) {
+		
+		String s = (String) JOptionPane.showInputDialog( 
+				window, 
+				"Please, choose your favorit language.", 
+				"Change Language", 
+				JOptionPane.PLAIN_MESSAGE, 
+				null, 
+				languages, "->" );
+		
+		if( ( s != null ) && ( s.length() > 0 ) ) {
+			
+			try {
+				Lang.setLanguage( s );
+			
+			} catch( Exception e ) {
+				e.printStackTrace();
+			}
+
+			return;
 		}
 	}
 }
